e64d65d4adc29422cdc17f26d4333f00866a1477
Banjoko Abdulazeez <seunbanjoko2005@yahoo.com>
2020-11-15 22:58:16 +0100

    Shared context

---
+++ spec/shared_context_spec.rb
@@ -0,0 +1,29 @@
+RSpec.shared_context "common" do
+    before do
+        @foods = []
+    end
+
+    def some_helper_method
+        5
+    end
+
+    let(:some_variable) { [1,2,3] }
+end
+
+RSpec.describe "first example group" do
+    include_context "common"
+
+    it "can use instance variables" do
+        expect(@foods.length).to eq(0)
+        @foods << "Amala"
+        expect(@foods.length).to eq(1)
+    end
+
+    it "can reuse instance variables across examples" do
+        expect(@foods.length).to eq(0)
+    end
+
+    it "can use shared helper methods" do
+        expect(some_helper_method).to eq(5)
+    end
+end
